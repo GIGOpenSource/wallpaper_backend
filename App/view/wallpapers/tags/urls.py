ad6fc6af6c4c53c3c7f2c364a19fd942a4969c7a
梁海波 <13751779+liangwater@user.noreply.gitee.com>
2026-04-13 13:46:08 +0800

    标签

--- App/view/wallpapers/tags/urls.py
+++ App/view/wallpapers/tags/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from App.view.wallpapers.tags.view import WallpaperTagViewSet
+from App.view.wallpapers.tags.view import WallpaperTagViewSet, NavigationTagViewSet
 
 router = DefaultRouter()
 router.register(r'', WallpaperTagViewSet, basename='wallpaper-tag')
+router.register(r'navigation_tag', NavigationTagViewSet, basename='navigation_tag')
 
 urlpatterns = [
     path('', include(router.urls)),
--- App/view/wallpapers/urls.py
+++ App/view/wallpapers/urls.py
@@ -10,12 +10,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from App.view.wallpapers.view import WallpapersViewSet, NavigationTagViewSet
+from App.view.wallpapers.view import WallpapersViewSet
 from App.view.wallpapers.tags import urls as tags_urls
 
 router = DefaultRouter()
 router.register(r'wallpaper', WallpapersViewSet, basename='wallpaper')
-router.register(r'navigation_tag', NavigationTagViewSet, basename='navigation_tag')
+
 urlpatterns = [
     path('', include(router.urls)),
     path('tags/', include(tags_urls)),
