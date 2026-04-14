a787f534edea9778a3a06e16309f86cd429ff70a
梁海波 <13751779+liangwater@user.noreply.gitee.com>
2026-04-14 11:00:15 +0800

    上传

--- App/view/customer/urls.py
+++ App/view/customer/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from App.view.customer.view import CustomerUserViewSet
-
+from tool.uploader_data import UploadResourceView
 router = DefaultRouter()
 router.register(r"users", CustomerUserViewSet, basename="customer_user")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("upload-image/", UploadResourceView.as_view(), name="upload-common-image"),
+
 ]
